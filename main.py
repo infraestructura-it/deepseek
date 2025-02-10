ca44415a5c4e0ccbb3433c28e9ec35652a72132a
Jairo Sepulveda C <30697670+infraestructura-it@users.noreply.github.com>
2025-02-10 19:14:20 +0000

    nuevo 2

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # Please install OpenAI SDK first: `pip3 install openai`
 
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv
 
 client = OpenAI(api_key="sk-41797a635e3c4cd0b63d1013f35ad665", base_url="https://api.deepseek.com")
-
+input_text = input ("enter your text")
 response = client.chat.completions.create(
     model="deepseek-chat",
+    store=True,
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
         {"role": "user", "content": "Hello"},
